9dec95915a7bdcf3ea2896025aee52a77a8f70de
koleosyama <koleosyamazaki@gmail.com>
2023-03-16 14:10:52 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -
+ *
+ * return : 0 Success
+ */
+int main(void)
+{
+	printf("Size of a char:\n",sizeof(char));
+	printf("Size of an int:\n",sizeof(int));
+	printf("Size of a long int:\n",sizeof(long int));
+	printf("Size of a long long int:\n",sizeof(long long int));
+	printf("Size of a float:",sizeof(float));
+	return(0);
+}
