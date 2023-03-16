b49bb0b1c7212c38548a8cab5141f8d7e14b49e9
koleosyama <koleosyamazaki@gmail.com>
2023-03-16 11:21:27 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main(void)
+{
+	char s[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+}
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+char s[]="hellow world";
+puts(s);
+return 0;
+}
