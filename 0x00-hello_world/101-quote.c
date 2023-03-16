ab1d7cabb5f50d2c1eda7dabc9109eda052afc01
koleosyama <koleosyamazaki@gmail.com>
2023-03-16 15:28:50 -0700

    fix3:101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - prints "and that piece of art is useful\" - Dora K.."
  *
- * Return: 0 Success
+ * Return: 1 Success
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (1);
 }
