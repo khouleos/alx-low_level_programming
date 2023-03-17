a15477597f66db3fae1fcfc64118393638ca6603
koleosyama <koleosyamazaki@gmail.com>
2023-03-17 11:49:08 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		putchar(i);
+		if (i == 57)
+                        break;
+		putchar(44);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
