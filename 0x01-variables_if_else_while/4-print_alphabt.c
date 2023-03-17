b1b7087104da7bb3b4c6f3d952d5164d91ba6fbf
koleosyama <koleosyamazaki@gmail.com>
2023-03-17 10:13:01 -0700

    4-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry pont of program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+	{
+
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
