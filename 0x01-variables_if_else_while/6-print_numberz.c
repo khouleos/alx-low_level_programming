6a999a5ad7b65609343035835822c1d7b51b27f9
koleosyama <koleosyamazaki@gmail.com>
2023-03-17 11:24:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * return: Always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
