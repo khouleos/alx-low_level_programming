9d5f98e6cff88ab551c2f3c19832430fe1dac139
koleosyama <koleosyamazaki@gmail.com>
2023-03-17 09:53:46 -0700

    fix:2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry pont of program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97 ; i <= 122 ; i++)
+		putchar(i);
+	return (0);
+}
