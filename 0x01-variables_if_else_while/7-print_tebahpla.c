051bf78e4000d92903bb3b29f443313abf5f8b4e
koleosyama <koleosyamazaki@gmail.com>
2023-03-17 11:30:44 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry pont of program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97 ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
