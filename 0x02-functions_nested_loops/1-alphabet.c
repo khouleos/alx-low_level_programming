a472e8e2bc7716c6d3528ebe5db2ba75ad7963b6
koleosyama <koleosyamazaki@gmail.com>
2023-03-21 14:55:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write a to z
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
+}
