df41ba0cc539ff0b6127ed61872b193dbd059369
koleosyama <koleosyamazaki@gmail.com>
2023-03-21 15:09:03 -0700

    fix:2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *
- *
+ * print_alphabet_x10 - print alphabet x10
+ * Return: void
  */
 
 void print_alphabet_x10(void)
@@ -11,8 +11,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for(ch = 'a'; ch <= 'z' ; ch++)
+		for (ch = 'a'; ch <= 'z' ; ch++)
+		{
 			_putchar(ch);
-	_putchar('\n')
+		}
+		_putchar('\n')
 	}
 }
