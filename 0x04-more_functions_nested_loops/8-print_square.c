7abf47cf9c86e4ba5fb61b583e3f80f65da6585b
koleosyama <koleosyamazaki@gmail.com>
2023-03-23 16:31:46 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,15 +5,20 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
+
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-	for (i = 0; i <= size ; i++)
+	for (i = 0; i < size ; i++)
 	{
-		_putchar(35);
+	for (j = 0; j < size ; j++)
+	{
+		_putchar('#');
+	}
 		_putchar('\n');
 	}
 	}
+	_putchar('\n');
 }
