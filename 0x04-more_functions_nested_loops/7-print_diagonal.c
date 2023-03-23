8b9521c1925f9c4f58d00642923a4df6ed319ec1
koleosyama <koleosyamazaki@gmail.com>
2023-03-23 15:16:40 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal n times
+ * @n: number of diagonal should be printed
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 1 ; i <= n ; i++)
+	{
+		_putchar(' ');
+		if (n < 0)
+		{
+			_putchar('\n');
+			break;
+		}
+	}
+	_putchar('\\');
+	_putchar('\n');
+		
+	
