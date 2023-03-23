bbbab708eb8875e1b562f11fa35425a2b159bac6
koleosyama <koleosyamazaki@gmail.com>
2023-03-23 16:09:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,16 +5,20 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 1 ; i <= n ; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar(' ');
-		if (n < 0)
-		{
-			_putchar('\n');
-			break;
-		}
+	for (i = 1; i <= n ; i++)
+	{
+	for (j = 1 ; j <= i ; j++)
+	{
+	_putchar(' ');
 	}
 	_putchar('\\');
 	_putchar('\n');
+	}
+	}
+}
