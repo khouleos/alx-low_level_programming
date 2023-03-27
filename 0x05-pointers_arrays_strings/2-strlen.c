53a9e8e668697ec0e1a8e53bf3216326ae15b837
koleosyama <koleosyamazaki@gmail.com>
2023-03-27 14:53:10 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *
- *
+ * _strlen - size of character
+ * @s: pointer
+ * Return: return results
  *
  */
 int _strlen(char *s)
 {
-	return sizeof(*s);
+	return (sizeof(s));
 }
