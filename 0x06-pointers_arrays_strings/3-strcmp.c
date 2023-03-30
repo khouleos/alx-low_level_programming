ec1367d3f87e7444f9fc996f586658db17e7b7b8
koleosyama <koleosyamazaki@gmail.com>
2023-03-30 07:51:08 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcmp - campre two string
+ * @s1: first string or character
+ * @s2: second string or character
+ * Return: print results of strcamp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
