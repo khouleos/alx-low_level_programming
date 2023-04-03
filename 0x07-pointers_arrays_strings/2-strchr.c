2d80fd735fe828adcd3d7bc2b9b0b9835c087696
koleosyama <koleosyamazaki@gmail.com>
2023-04-03 08:14:49 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - function to search
+ * @s: string
+ * @c: searched character
+ * Return: print results if found or print null if not
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (s == c)
+			return (s);
+	s++;
+	}
+	return (0);
+}
