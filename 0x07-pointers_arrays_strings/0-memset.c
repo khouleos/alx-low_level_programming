2d702705bc488815c16bb28183dfa5d17f918924
koleosyama <koleosyamazaki@gmail.com>
2023-04-03 07:14:58 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - fill memory with bytes
+ * @s: memory address where to set new values
+ * @b: new value to set
+ * @n: number of bytes
+ * Return: print results
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+		s[i] = b;
+	return (s);
+}
