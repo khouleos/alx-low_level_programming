0482c242f9e4040a70a7f2d9e1b9018f4feed561
koleosyama <koleosyamazaki@gmail.com>
2023-04-03 07:55:00 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	if (dest == 0)
-		return (0);
-	for (i = 0 ; i < n - 1 ; i++)
+	for (i = 0 ; i < n ; i++)
 	dest[i] = src[i];
-	dest[i] = '\n';
 	return (dest);
 }
