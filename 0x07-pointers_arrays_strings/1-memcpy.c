2d3c5382dbe5522ba23e20f26b0aee7e263d331c
koleosyama <koleosyamazaki@gmail.com>
2023-04-03 07:39:06 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function to copy address from src to dest
+ * @dest: destination adress
+ * @src: source address
+ * @n: number of bytes to be copied
+ * Return: print results
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n - 1 ; i++)
+	dest[i] = src[i];
+	dest[i] = '\n';
+	return (dest);
+}
