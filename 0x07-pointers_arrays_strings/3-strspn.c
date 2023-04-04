207eae0ce038ddddbb04147b9a012c4238520f67
koleosyama <koleosyamazaki@gmail.com>
2023-04-04 03:11:24 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - function returns the length of the initial substring of the string
+ * @s: string to be scanned
+ * @accept: string  containing characters to match
+ * Return: return the number of matched char
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int n = 0;
+
+	for (j = 0 ; s ; j++)
+	{
+	for (i = 0 ; accept[i] ; i++)
+	{
+	if (*s == accept[i])
+	{
+	n++;
+	break;
+	}
+	else if (accept[i + 1] == '\0')
+	return (n);
+
+	}
+	s++;
+	}
+	return (n);
+}
