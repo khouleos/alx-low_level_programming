1ee5a6f00b90fd490bd37fb8d10c49010687edb0
koleosyama <koleosyamazaki@gmail.com>
2023-04-04 17:28:25 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,10 +11,23 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == 0)
 		return (haystack);
-	for (i = 0; haystack[i] ; i++)
+	while (haystack)
 	{
+		i = 0;
+
 		if (haystack[i] == needle[i])
-			return (haystack + i);
+		{
+				do {
+
+				if (needle[i + 1] == '\0')
+				return (haystack);
+				i++;
+
+				} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+
 	}
 	return (0);
+
 }
