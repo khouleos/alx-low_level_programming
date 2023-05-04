50eee729227461b7d311f29e27f85816e0c80a90
Kholeos <bennahkhalil@gmail.com>
2023-05-04 15:00:14 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,8 +8,11 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int temp = 1ul << index;
+	unsigned long int x = sizeof(unsigned long int) * 8;
 
-	if (index >= sizeof(unsigned long int) * 8)
-			return (-1);
+	if (index >= x)
+	{
+		return (-1);
+	}
 	return ((n & temp) ? 1 : 0);
 }
