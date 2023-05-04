67473b8204339e0bfc309de4e830e7ac2d2e3142
Kholeos <bennahkhalil@gmail.com>
2023-05-04 15:32:47 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1ul << index;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	return (-1);
-	unsigned long int mask = 1ul << index;
 	*n = *n & ~mask;
 	return (1);
 }
