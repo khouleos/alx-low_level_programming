fad9e3bfc3c0fa3bb04c7d418549693e65a27549
Kholeos <bennahkhalil@gmail.com>
2023-05-08 16:01:34 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t op, rd, wr;
-	char buff;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
-	op = open(filename, O_RONLY);
+
+	op = open(filename, O_RDONLY);
 	rd = read(op, buff, letters);
-	wr = write(STDOUT_FILENO, buff, r);
+	wr = write(STDOUT_FILENO, buff, rd);
+
 	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buff);
-		close(op);
+		return (0);
 	}
+
+	free(buff);
+	close(op);
+
 	return (wr);
 }
